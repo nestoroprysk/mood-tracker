784798f288615f3b06243bf26a9c049900602689
Nestor <nestoroprysk@gmail.com>
2021-11-14 21:15:26 +0200

    Implementing /stat to return a JSON

--- internal/cmd/stat.go
+++ internal/cmd/stat.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 
 	"github.com/nestoroprysk/mood-tracker/internal/repository"
@@ -8,6 +9,21 @@ import (
 
 func newStat(r repository.Repository, userID int, args ...string) (Cmd, error) {
 	return func() (string, error) {
-		return fmt.Sprintf("%v", args), nil
+		b, err := r.Read(userIDJSON(userID))
+		if err != nil {
+			return "", err
+		}
+
+		var reg Registry
+		if err := json.Unmarshal(b, &reg); err != nil {
+			return "", err
+		}
+
+		b, err = json.MarshalIndent(reg, "", " ")
+		if err != nil {
+			return "", err
+		}
+
+		return fmt.Sprintf("%s", b), nil
 	}, nil
 }
