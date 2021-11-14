feced708da9e79699493c7f23591e906ea12d282
Nestor <nestoroprysk@gmail.com>
2021-11-15 01:37:21 +0200

    Adding frequencies

--- internal/cmd/stat.go
+++ internal/cmd/stat.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"io"
+	"strconv"
 	"time"
 
 	chart "github.com/wcharczuk/go-chart/v2"
@@ -21,22 +22,22 @@ func newStat(c config) (Cmd, error) {
 			return "", err
 		}
 
-		png, err := timePNG(r)
-		if err != nil {
-			return "", err
-		}
+		for _, e := range []struct {
+			name string
+			f    func(Registry) (io.Reader, error)
+		}{
+			{name: "time.png", f: timePNG},
+			{name: "label.png", f: labelPNG},
+			{name: "freq.png", f: freqPNG},
+		} {
+			png, err := e.f(r)
+			if err != nil {
+				return "", err
+			}
 
-		if _, err := c.SendPNG("time.png", png); err != nil {
-			return "", err
-		}
-
-		png, err = labelPNG(r)
-		if err != nil {
-			return "", err
-		}
-
-		if _, err := c.SendPNG("label.png", png); err != nil {
-			return "", err
+			if _, err := c.SendPNG(e.name, png); err != nil {
+				return "", err
+			}
 		}
 
 		return "Enjoy!", nil
@@ -70,7 +71,7 @@ func timePNG(r Registry) (io.Reader, error) {
 }
 
 func labelPNG(r Registry) (io.Reader, error) {
-	var m map[string]int
+	m := map[string]int{}
 	for _, i := range r {
 		for _, l := range i.Labels {
 			m[l]++
@@ -87,6 +88,47 @@ func labelPNG(r Registry) (io.Reader, error) {
 
 	graph := chart.BarChart{
 		Title: "Mood Labels",
+		Background: chart.Style{
+			Padding: chart.Box{
+				Top: 40,
+			},
+		},
+		Height:   512,
+		BarWidth: 60,
+		Bars:     vals,
+	}
+
+	b := &bytes.Buffer{}
+	if err := graph.Render(chart.PNG, b); err != nil {
+		return nil, err
+	}
+
+	return b, nil
+}
+
+func freqPNG(r Registry) (io.Reader, error) {
+	m := map[int]int{
+		0: 0,
+		1: 0,
+		2: 0,
+		3: 0,
+		4: 0,
+		5: 0,
+	}
+	for _, i := range r {
+		m[i.Mood]++
+	}
+
+	var vals []chart.Value
+	for k, v := range m {
+		vals = append(vals, chart.Value{
+			Label: strconv.Itoa(k),
+			Value: float64(v),
+		})
+	}
+
+	graph := chart.BarChart{
+		Title: "Mood Frequencies",
 		Background: chart.Style{
 			Padding: chart.Box{
 				Top: 40,
